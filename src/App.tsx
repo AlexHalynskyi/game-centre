7f4ee556730cdc1d510f79e9a7fa897987b58cbf
OHalynskyi <ohalynskyi@gmail.com>
2022-12-29 18:45:52 +0200

    Added redux and cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { Provider} from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 import { router } from '@/router';
 import { Loader } from '@/components';
 import { Context, useFirebase } from '@/auth';
+import { store } from '@/store';
 
 function App() {
   return (
-    <Context.Provider value={useFirebase()} >
-      <Suspense fallback={<Loader/>}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </Context.Provider>
+    <CookiesProvider>
+      <Provider store={store}>
+        <Context.Provider value={useFirebase()} >
+          <Suspense fallback={<Loader/>}>
+            <RouterProvider router={router} />
+          </Suspense>
+        </Context.Provider>
+      </Provider>
+    </CookiesProvider>
   );
 }
 
--- src/components/SignInMail/SignInMail.tsx
+++ src/components/SignInMail/SignInMail.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -7,14 +8,25 @@ import { useNavigate } from 'react-router-dom';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { signInSchema } from '@/components/SignInMail';
 import { HOME_PATH } from '@/router';
+import { setUser } from '@/store/slices/userSlice';
+import { useAppDispatch } from '@/hooks/redux-hooks';
 
 const SignInMail = () => {
+  const dispatch = useAppDispatch();
+  const [, setCookie] = useCookies<string>([]);
   const auth  = getAuth();
   const navigate = useNavigate();
 
   const signIn = async (email: string, password: string) => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      const { user } = await signInWithEmailAndPassword(auth, email, password);
+      const token = await user.getIdToken();
+      dispatch(setUser({
+        email: user.email,
+        id: user.uid,
+        token,
+      }));
+      setCookie('accessToken', token);
       navigate(HOME_PATH);
     } catch (e: unknown) {
       console.log(e);
